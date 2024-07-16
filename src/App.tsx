b6ef101210c39d92f4bc224b8504fc5ab2472af0
ronaldjeq9512 <ronaldjhairestelaquintana@gmail.com>
2024-07-15 21:45:38 -0500

    refatoring Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-import './App.css'
-import { Footer } from './components/Footer/footer';
-import { Card } from './components/Card/card1';
-import Json from './../db.json';
-import { Header } from './components/Header';
-import { Carousel } from './components/Carousel';
+import './App.css';
 import { PrincipalNavigator } from './navigators/PrincipalNavigator';
 
-const cards = [];
-
-Json.products.forEach(function (e) {
-  cards.push(
-    <Card
-      title={e.title}
-      descripción={e.descripción}
-      price={e.price}
-    />
-  )
-});
-
 function App() {
-
-return (
-/*   <>
-   <Header />
-   <Carousel />
-   <section className='d-flex flex-wrap justify-content-center align-item-center'>
-          {cards}
-    </section>
-   
-    <Footer />
-  </> */
-    <PrincipalNavigator />
-)
+  return <PrincipalNavigator />;
 }
 
-
-export default App
+export default App;
