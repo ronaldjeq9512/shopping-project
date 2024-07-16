15efe347211622e598f38ecaa28d6041fd34236b
ronaldjeq9512 <165746925+ronaldjeq9512@users.noreply.github.com>
2024-07-15 21:11:02 -0500

    update header (#19)

--- src/constants/carousel.ts
+++ src/constants/carousel.ts
@@ -1,4 +1,3 @@
-import Promo1 from '../assets/images/carousel/promo1.webp'
 import Promo2 from '../assets/images/carousel/promo2.webp'
 import Promo3 from '../assets/images/carousel/promo3.webp'
 
@@ -6,7 +5,6 @@ import Promo3 from '../assets/images/carousel/promo3.webp'
 
 
 export const CAROUSEL = [
-    Promo1,
     Promo2,
     Promo3
 ]
