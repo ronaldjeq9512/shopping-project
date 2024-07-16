fbd6c580cfd68075ca7145cd8f5008bbe4490d06
Katherin Anampa <sv46114380@idat.edu.pe>
2024-07-16 17:49:17 -0500

    boton de card

--- src/constants/carousel.ts
+++ src/constants/carousel.ts
@@ -1,10 +1,11 @@
 import Promo2 from '../assets/images/carousel/promo2.webp'
 import Promo3 from '../assets/images/carousel/promo3.webp'
-
+import Promo4 from '../assets/images/carousel/promo4.jpg'
 
 
 
 export const CAROUSEL = [
     Promo2,
-    Promo3
+    Promo3,
+    Promo4
 ]
